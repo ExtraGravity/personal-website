b4d8a11da211d13f3c37d07d792a0520e1ebb681
Enoch Tsang <etsang1@hotmail.com>
2017-03-28 03:01:52 -0600

    content out transition

--- resources/js/base.js
+++ resources/js/base.js
@@ -1,3 +1,9 @@
+var MOBILE = false
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    MOBILE = true;
+}
+
 $("document").ready(function(){
     easeInHeader();
     insertContent();
@@ -7,7 +13,6 @@ $("document").ready(function(){
 });
 
 function easeInHeader() {
-     var filterVal = "filter(0)";
     $( ".header-bg" ).css({
         "opacity":"1",
         "transition":"opacity 0.8s ease-out",
@@ -20,25 +25,39 @@ function easeInHeader() {
 }
 
 function insertContent() {
-    $(".content").hide().fadeIn("slow");
+    $(".content-wrapper").hide().fadeIn("slow");
 }
 
 function fillPlaceholder(){
     var str = "start exploring";
-    var i = 0;
 
-    (function type() {
-        $(".nav-bar input").attr("placeholder", str.slice(0, ++i)); 
-        if ($(".nav-bar input").attr("placeholder") === str) return;
-        setTimeout(type, 150);
-    }());
+    if(MOBILE){
+        $(".nav-bar input").attr("placeholder", str);
+    } else {    
+        var i = 0;
+
+        (function type() {
+            $(".nav-bar input").attr("placeholder", str.slice(0, ++i)); 
+            if ($(".nav-bar input").attr("placeholder") === str) return;
+            setTimeout(type, 150);
+        }());
+    }
 }
 
 function fadeInFooter() {
     $("footer div").fadeIn("fast");
 }
 
+function closeNavigation() {
+    $(".navigation-icon").fadeOut(300);
+    $(".navigation-background").delay(200).slideUp(300);
+}
+
 function setupNavbarEvents() {
+    if(MOBILE) {
+        $(".nav-bar input").attr("readonly", "true");
+    }
+
     $(".nav-bar input").click(function(){
         if (!$(".navigation-icon").is(":visible")) {
             $(".navigation-background").slideDown(300);
@@ -55,8 +74,53 @@ function setupNavbarEvents() {
         if (!container.is(e.target) // if the target of the click isn"t the container...
             && container.has(e.target).length === 0) // ... nor a descendant of the container
         {
-            $(".navigation-icon").fadeOut(300);
-            $(".navigation-background").delay(200).slideUp(300);
+            closeNavigation();
         }
     });    
+
+    var navCloseDelay = 250;
+    if(MOBILE) {
+        navCloseDelay = 700;
+    }
+
+    $(".about").click(function() {
+        setTimeout(closeNavigation, navCloseDelay);
+        removeContent();
+    });
+
+    $(".portfolio").click(function() {
+        setTimeout(closeNavigation, navCloseDelay);
+        removeContent();
+    });
+
+    $(".contact").click(function() {
+        setTimeout(closeNavigation, navCloseDelay);
+        removeContent();
+    });
+
+    $(".articles").click(function() {
+        setTimeout(closeNavigation, navCloseDelay);
+        removeContent();
+    });
+
+    $(".resume").click(function() {
+        setTimeout(closeNavigation, navCloseDelay);
+        removeContent();
+    });
+}
+
+
+function removeContent() {
+    $( ".content" ).css({
+        "opacity": "0",
+        "-webkit-filter": "blur(5px)",
+        "-moz-filter": "blur(5px)",
+        "-o-filter": "blur(5px)",
+        "-ms-filter": "blur(5px)",
+        "filter": "blur(5px)",
+        "transition":"filter 1s linear, opacity 1s linear",
+        "-webkit-transition":"filter 1s linear, opacity 1s linear",
+        "-moz-transition":"filter 1s linear, opacity 1s linear",
+        "-o-transition":"filter 1s linear, opacity 1s linear",
+    });
 }
