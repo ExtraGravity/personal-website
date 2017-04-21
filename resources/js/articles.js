fb5c2f085a102c7746a415e061dfe43849a5b519
Enoch Tsang <etsang1@hotmail.com>
2017-04-20 18:15:57 -0600

    article listing styling

--- resources/js/articles.js
+++ resources/js/articles.js
@@ -1,8 +1,17 @@
 $("document").ready(function(){
     $(".articles-list a").each(function() {
     	$(this).on("click", function() {
+    		$(this).css("color", "#DDD");
+    		$(this).css("background", "#444");
+    		var delay = 0;
+    		if(MOBILE) {
+    			delay = 400;
+    		}
     		var pageToReq = $(this).attr("url");
-            changeContent(pageToReq)
+    		
+    		setTimeout(function() {
+            	changeContent(pageToReq);
+    		}, delay);
     	})
     })
 });
