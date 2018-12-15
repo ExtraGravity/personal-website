b74abaf154ec96d9cda94e1123f9c92dbe0af29f
Enoch Tsang <etsang1@hotmail.com>
2018-12-15 01:35:11 -0700

    fix docker build on windows adding random ;C

--- docker-build.sh
+++ docker-build.sh
@@ -6,21 +6,21 @@ DOCKER_HOME="/go/src/"
 ARG_NUM=2
 
 if [ "$1" == "init" ]; then
-	docker build -t $IMAGE_NAME $DIR
+    docker build -t $IMAGE_NAME $DIR
 elif [ "$1" == "build" ]; then
-	docker run --rm -v $DIR:/go/src/github.com/enochtsang/personal-website \
-		$IMAGE_NAME ./build.sh "${@:2}"
+    echo $DIR
+    docker run --rm -v /$DIR:/go/src/github.com/enochtsang/personal-website \
+        $IMAGE_NAME ./build.sh "${@:2}"
 elif [ "$1" == "run" ]; then
-	./docker-build.sh build
-	PORT="8000"
-	if [ ! -z "$2" ]; then
-		PORT=$2
-	fi
-	echo ""
-	echo "STARTING WEBSITE ON PORT $PORT"
-	docker run --rm -v $DIR:/go/src/github.com/enochtsang/personal-website \
-		-p $PORT:$PORT $IMAGE_NAME ./run.sh $PORT
+    ./docker-build.sh build
+    PORT="8000"
+    if [ ! -z "$2" ]; then
+        PORT=$2
+    fi
+    echo ""
+    echo "STARTING WEBSITE ON PORT $PORT"
+    docker run --rm -v $DIR:/go/src/github.com/enochtsang/personal-website \
+        -p $PORT:$PORT $IMAGE_NAME ./run.sh $PORT
 else
-	echo "usage: docker_build [init | build | run]"
+    echo "usage: docker_build [init | build | run]"
 fi
-
