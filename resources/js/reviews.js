27830c44048d2aa672c2bd8efeb83eb6183d6fe7
Enoch Tsang <etsang1@hotmail.com>
2022-09-05 03:38:39 -0400

    Add anime reviews drop downs
    Made pentagon text unselectable
    Fixed animated jquery buttons highlight blue when clicked

--- resources/js/reviews.js
+++ resources/js/reviews.js
@@ -3,4 +3,9 @@ $("document").ready(function(){
     	let reviewPage = "articles/" + $(this).attr('internal-href');
         changeContent(reviewPage, true);
     })
+
+    $(".clickable-row").on("click", function() {
+    	$(this).find(".anime-review-blurb").slideToggle();
+    	$(this).find('svg').toggleClass('rotate-down');
+    });
 });
