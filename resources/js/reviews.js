149fc021da6d26794a2e4aa6fffbd892d232b505
Enoch Tsang <etsang1@hotmail.com>
2026-01-26 19:27:07 -0700

    Fix zebra striping

--- resources/js/reviews.js
+++ resources/js/reviews.js
@@ -4,6 +4,14 @@ $("document").ready(function(){
         changeContent(reviewPage, true, true);
     })
 
+    $(".anime-games-table").each(function() {
+        const rows = $(this).children(".anime-games-table-row");
+        rows.removeClass("row-odd row-even");
+        rows.each(function(index) {
+            $(this).addClass(index % 2 === 0 ? "row-odd" : "row-even");
+        });
+    });
+
     $(".clickable-row").on("click", function() {
     	$(this).find(".anime-games-review-blurb").slideToggle();
     	$(this).find('svg').toggleClass('rotate-down');
